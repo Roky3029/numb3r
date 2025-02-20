40f9d88fb5e987d9acfa3cd0fd4e79d540cef0f7
Roky3029 <mikirb30@gmail.com>
2025-02-20 18:57:07 +0100

    commented the About section as it will not be used for the moment

--- src/components/implementations/properties/About.tsx
+++ src/components/implementations/properties/About.tsx
@@ -1,19 +1,19 @@
-import parse from 'html-react-parser'
+// import parse from 'html-react-parser'
 
-interface AboutProps {
-	title: string
-	about: string
-}
+// interface AboutProps {
+// 	title: string
+// 	about: string
+// }
 
-export default function About({ title, about }: AboutProps) {
-	return (
-		// <div className='bg-slate-300 text-black py-5 mt-20 rounded-lg mb-10'>
-		// 	<p className='text-4xl uppercase border-b-2 px-10 border-slate-800'>
-		// 		{title}
-		// 	</p>
+// export default function About({ title, about }: AboutProps) {
+// 	return (
+// 		// <div className='bg-slate-300 text-black py-5 mt-20 rounded-lg mb-10'>
+// 		// 	<p className='text-4xl uppercase border-b-2 px-10 border-slate-800'>
+// 		// 		{title}
+// 		// 	</p>
 
-		// 	<p className='prose lg:prose-xl pl-10 py-6'>{parse(about)}</p>
-		// </div>
-		<></>
-	)
-}
+// 		// 	<p className='prose lg:prose-xl pl-10 py-6'>{parse(about)}</p>
+// 		// </div>
+// 		<></>
+// 	)
+// }
